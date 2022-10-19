d439be76d9b571c1b1e00afa28a48e05c0247bb3
Lee-moran <leemoran06@hotmail.com>
2022-10-19 16:35:01 +0000

    added a custom context hook to refactoring

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -14,9 +14,12 @@ import { Link, useHistory } from "react-router-dom";
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
-import {backend} from '../../constant';
+import { useSetCurrentUser } from "../../context/CurrentUserContext";
+import {backend} from "../../constant";
 
 function SignInForm() {
+  const setCurrentUser = useSetCurrentUser();
+
   const [signInData, setSignInData] = useState({
     username: "",
     password: "",
@@ -28,8 +31,10 @@ function SignInForm() {
   const history = useHistory();
   const handleSubmit = async (event) => {
     event.preventDefault();
+
     try {
-      await axios.post(backend + "/dj-rest-auth/login/", signInData);
+      const { data } = await axios.post(backend + "/dj-rest-auth/login/", signInData);
+      setCurrentUser(data.user);
       history.push("/");
     } catch (err) {
       setErrors(err.response?.data);
