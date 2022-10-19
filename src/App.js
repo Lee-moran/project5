df08f18127d35276450414151072650f80708067
Lee-moran <leemoran06@hotmail.com>
2022-10-19 14:40:08 +0000

    api1

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 
+
 function App() {
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render= {() => <h1>Home</h1>} />
-          <Route exact path="/about" render= {() => <h1>about</h1>} />
-          <Route exact path="/signin" render= {() => <h1>Signin</h1>} />
-          <Route exact path="/signup" render= {() => <SignUpForm/> } />
-          <Route render={() => <p>Page not found!</p>} />
-        </Switch>
-      </Container>
-    </div>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/about" render={() => <h1>about</h1>} />
+          <Route exact path="/signin" render={() => <h1>Signin</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm/> } />
+        <Route render={() => <p>Page not found!</p>} />
+      </Switch>
+    </Container>
+    </div >
   );
 }
 
