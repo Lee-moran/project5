1f53039f97bcaf1a00a8df882df4a299bd96b545
Lee-moran <leemoran06@hotmail.com>
2022-10-20 08:48:52 +0000

    updated

--- src/context/CurrentUserContext.js
+++ src/context/CurrentUserContext.js
@@ -16,7 +16,7 @@ export const CurrentUserProvider = ({ children }) => {
 
   const handleMount = async () => {
     try {
-      const { data } = await axios.get(backend + "dj-rest-auth/user/");
+      const { data } = await axios.get(backend + "dj-rest-auth/user");
       setCurrentUser(data);
     } catch (err) {
       console.log(err);
