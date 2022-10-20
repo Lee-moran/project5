52a958416c5dc42d39ccd88e83d6a778c63ec39b
Lee-moran <leemoran06@hotmail.com>
2022-10-20 08:33:54 +0000

    imported default profile icon

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import styles from "../styles/Avatar.module.css";
+import icon from "../assets/icon.jpg";
 
 const Avatar = ({ src, height = 45, text }) => {
   return (
     <span>
       <img
         className={styles.Avatar}
-        src={src}
+        src={icon}
         height={height}
         width={height}
         alt="avatar"
